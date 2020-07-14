2ce0898e746a192048f8f14ba9d29e79c705e93a
johnson_phillips <johnson.phillips@mheducation.com>
2020-07-14 11:25:01 -0400

    changed selenium webdriver version to 3.6.0

--- tests/uitest.ts
+++ tests/uitest.ts
@@ -1,11 +1,17 @@
 import Browser from "../src/browser";
 import {By, Capabilities, Key} from "selenium-webdriver";
+import report from "automation.report";
 
 async function test() {
+    report.deleteReportFolder();
+    report.startTest("sample ui test","test to validate ui library");
     let chrome =  new Browser(Capabilities.chrome());
-    await chrome.driver.get('https://www.npmjs.com/package/qe.automation.ui');
-    await chrome.driver.findElement(By.name('q')).sendKeys('qe.automation.ui',Key.RETURN);
+    await chrome.driver.get('https://www.npmjs.com/package/automation.ui');
+    await report.addTestStep("launched https://www.npmjs.com/package/automation.ui",null,chrome.driver);
+    await chrome.driver.findElement(By.name('q')).sendKeys('automation.ui',Key.RETURN);
+    await report.addTestStep("entered automation.ui search text",null,chrome.driver);
     await chrome.driver.close();
+    await report.endTest();
 }
 
 (async () => {
